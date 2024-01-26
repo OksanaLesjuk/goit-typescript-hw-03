e2914b386602e9270a4039621cc835b72b78bc45
OksanaLesjuk <126483028+OksanaLesjuk@users.noreply.github.com>
2024-01-26 22:18:06 +0200

    4

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,43 @@
+class Key {
+    constructor(private signature: number = Math.random() ){}
+    getSignature():number{
+        return this.signature
+    }
+}
+
+class Person {
+
+    constructor(private key:Key){}
+       
+    getKey():Key{
+        return this.key
+    }
+}
+
+abstract class House {
+    door:boolean;
+    key:Key;
+    tenants:Person[] = []
+    comeIn(person:Person ):void{
+        if(this.door){ 
+             this.tenants.push(person)
+            }
+      
+    }
+
+    abstract openDoor(obj: Key):boolean
+}
+class MyHouse extends House {
+    constructor(key: Key) {
+        super();
+        this.key = key;
+    }
+    openDoor(obj: Key): boolean {
+        return obj.getSignature() === this.key.getSignature();
+    }
+}
+
+
 const key = new Key();
 
 const house = new MyHouse(key);
